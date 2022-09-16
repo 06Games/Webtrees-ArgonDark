3665266c715417fc51a4c2fe8c62380ea899c336
EvanG <evan_g@orange.fr>
2022-09-16 16:06:36 +0200

    Initial commit

---
+++ module.php
@@ -0,0 +1,16 @@
+<?php
+
+/************************************************
+                Argon Dark Theme
+             Originally made by jchue
+  Under the Internet Systems Consortium License
+  https://github.com/jchue/argon-webtrees-theme
+
+       Forked and maintained by Evan Galli
+             Under the same licence
+ https://github.com/06Games/Webtrees-ArgonDark
+***********************************************/
+
+namespace EvanG\Themes\DarkTheme;
+include "DarkModule.php";
+return new DarkTheme();
